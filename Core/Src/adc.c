3f3349c65f7fd1337ee68f94c8c9de6d1423fef5
ShunjiHashimoto <h.shunji1206@gmail.com>
2024-12-16 22:51:19 +0900

    3/4のフォトトランジスタの値が取れた

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -53,7 +53,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 2;
   hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
@@ -63,9 +63,19 @@ void MX_ADC1_Init(void)
 
   /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
   */
+  sConfig.Channel = ADC_CHANNEL_13;
+  sConfig.Rank = ADC_INJECTED_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
   sConfig.Channel = ADC_CHANNEL_9;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
--- Core/Src/photo_trans.cpp
+++ Core/Src/photo_trans.cpp
@@ -1,69 +1,110 @@
 #include "../../Core/Inc/photo_trans.hpp"
 
 void PhotoTransSensor::getWallSensorData() {
-    // for(int i=0; i<4; i++){
-    //     if(i == 0){
-    // ADC_ChannelConfTypeDef sConfig = {0};
-    // config
-    // sConfig.Channel = LL_ADC1_Pin;
-    // sConfig.Rank = 1;
-    // sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
-    // sConfig.Offset = 0;
-    // if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
-    //     printf("something wrong");
-    //     return;
-    // }
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    // IN9
+    sConfig.Rank = 1;
+    sConfig.Channel = ADC_CHANNEL_9;
+    sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+    sConfig.Offset = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
     HAL_GPIO_WritePin(LL_LED_GPIO_Port, LL_LED_Pin, GPIO_PIN_SET);
     for(int i=0; i<100; i++) {}
-    HAL_ADC_Start(&hadc1);
-    HAL_ADC_PollForConversion(&hadc1, 1000);
-    uint16_t adcValueOn = HAL_ADC_GetValue(&hadc1);
+    if(HAL_ADC_Start(&hadc1) == HAL_OK){
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOn = HAL_ADC_GetValue(&hadc1);
 
-    HAL_GPIO_WritePin(LL_LED_GPIO_Port, LL_LED_Pin, GPIO_PIN_RESET);
-    HAL_Delay(1);
-    HAL_ADC_Start(&hadc1);
-    HAL_ADC_PollForConversion(&hadc1, 1000);
-    uint16_t adcValueOff = HAL_ADC_GetValue(&hadc1);
+        HAL_GPIO_WritePin(LL_LED_GPIO_Port, LL_LED_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOff = HAL_ADC_GetValue(&hadc1);
 
-    adc_value = adcValueOn - adcValueOff;
-    printf("adc_val On[%d], Off[%d]: L L_ADC: %d \n\r", adcValueOn, adcValueOff, adc_value);
-    HAL_ADC_Stop(&hadc1);
-    HAL_Delay(500);
-        // }
-        // else if(i == 1){
-        //     led_sensor.blink(LF_LED_GPIO_Port, LF_LED_Pin);
-        //     for(int i=0; i<200; i++) {}
-        //     HAL_ADC_Start(&hadc1);
-        //     HAL_ADC_PollForConversion(&hadc1, 1);
-        //     adc_value = HAL_ADC_GetValue(&hadc1);
-        //     // adc_val[i] = 1000*3.3 * adc_value / 4096; // 値がわかりやすいようにx1000
-        //     led_sensor.stop(LF_LED_GPIO_Port, LF_LED_Pin);
-        //     HAL_ADC_Stop(&hadc1);
-        //     printf("adc_val [%d]: L F_ADC: %d \n\r", i, adc_value);
-        // }
-        // else if(i == 2){
-        //     led_sensor.blink(RF_LED_GPIO_Port, RF_LED_Pin);
-        //     for(int i=0; i<200; i++) {}
-        //     HAL_ADC_Start(&hadc1);
-        //     HAL_ADC_PollForConversion(&hadc1, 1);
-        //     adc_value = HAL_ADC_GetValue(&hadc1);
-        //     // adc_val[i] = 1000*3.3 * adc_value / 4096; // 値がわかりやすいようにx1000
-        //     led_sensor.stop(RF_LED_GPIO_Port, RF_LED_Pin);
-        //     HAL_ADC_Stop(&hadc1);
-        //     printf("adc_val, [%d]: R F _ADC: %d \n\r",i,  adc_value);
-        // }
-        // else if(i == 3){
-        //     led_sensor.blink(LL_LED_GPIO_Port, LL_LED_Pin);
-        //     for(int i=0; i<200; i++) {}
-        //     HAL_ADC_Start(&hadc1);
-        //     HAL_ADC_PollForConversion(&hadc1, 1);
-        //     adc_value = HAL_ADC_GetValue(&hadc1);
-        //     // adc_val[i] = 1000*3.3 * adc_value / 4096; // 値がわかりやすいようにx1000
-        //     led_sensor.stop(LL_LED_GPIO_Port, LL_LED_Pin);
-        //     HAL_ADC_Stop(&hadc1);
-        //     printf("adc_val [%d]: L L_ADC: %d \n\r", i, adc_value);
-        // }
-    // }
+        adc_value = adcValueOn - adcValueOff;
+        printf("adc_val On[%d], Off[%d]: L L_ADC: %d \n\r", adcValueOn, adcValueOff, adc_value);
+    }
+
+    // IN13
+    sConfig.Rank = 1;
+    sConfig.Channel = ADC_CHANNEL_13;
+    sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+    sConfig.Offset = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    HAL_GPIO_WritePin(RF_LED_GPIO_Port, RF_LED_Pin, GPIO_PIN_SET);
+    for(int i=0; i<100; i++) {}
+    if(HAL_ADC_Start(&hadc1) == HAL_OK){
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOn = HAL_ADC_GetValue(&hadc1);
+
+        HAL_GPIO_WritePin(RF_LED_GPIO_Port, RF_LED_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOff = HAL_ADC_GetValue(&hadc1);
+
+        adc_value = adcValueOn - adcValueOff;
+        printf("adc_val On[%d], Off[%d]: R F_ADC: %d \n\r", adcValueOn, adcValueOff, adc_value);
+    }
+
+    // IN15
+    sConfig.Rank = 1;
+    sConfig.Channel = ADC_CHANNEL_15;
+    sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+    sConfig.Offset = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    HAL_GPIO_WritePin(LF_LED_GPIO_Port, LF_LED_Pin, GPIO_PIN_SET);
+    for(int i=0; i<100; i++) {}
+    if(HAL_ADC_Start(&hadc1) == HAL_OK){
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOn = HAL_ADC_GetValue(&hadc1);
+
+        HAL_GPIO_WritePin(LF_LED_GPIO_Port, LF_LED_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOff = HAL_ADC_GetValue(&hadc1);
+
+        adc_value = adcValueOn - adcValueOff;
+        printf("adc_val On[%d], Off[%d]: L F_ADC: %d \n\r", adcValueOn, adcValueOff, adc_value);
+    }
+
+    // IN11
+    sConfig.Rank = 1;
+    sConfig.Channel = ADC_CHANNEL_11;
+    sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+    sConfig.Offset = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    HAL_GPIO_WritePin(RR_LED_GPIO_Port, RR_LED_Pin, GPIO_PIN_SET);
+    for(int i=0; i<100; i++) {}
+    if(HAL_ADC_Start(&hadc1) == HAL_OK){
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOn = HAL_ADC_GetValue(&hadc1);
+
+        HAL_GPIO_WritePin(RR_LED_GPIO_Port, RR_LED_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 100);
+        uint16_t adcValueOff = HAL_ADC_GetValue(&hadc1);
+
+        adc_value = adcValueOn - adcValueOff;
+        printf("adc_val On[%d], Off[%d]: R R_ADC: %d \n\r", adcValueOn, adcValueOff, adc_value);
+    }
+
+    printf("Finished!!\n\r");
+    HAL_Delay(200);
 }
 
 // led_sensor.blink();
